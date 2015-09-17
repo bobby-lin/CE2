2c536c4af8151af635803658606e0aeb4354c248
Bobby <Bobby@Bobby-Laptop>
2015-09-17 18:41:57 +0800

    Create UnitTest class for testing TextBuddy program

---
+++ src/UnitTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void testSort() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+}
